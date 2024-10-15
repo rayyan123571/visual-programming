5b8a4fa86a5409d94cee5f5595e1d3a798eb8cf0
rayyan123571 <rayyanch47@gmail.com>
2024-10-15 22:33:43 +0500

    Add files via upload

---
+++ 233532_lab_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _233532_lab_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This lab is Understanding the Visual studio");
+            Console.WriteLine();
+            Console.WriteLine("create a new project and open in visual studio");
+
+            Console.WriteLine("My name is Rayyan");
+            Console.WriteLine("My Registration id is 233532");
+            Console.ReadLine();
+        }
+    }
+}
