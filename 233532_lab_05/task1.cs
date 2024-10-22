660c7b7b7d1faac3cc347423e2d7b0e1df86fa37
Muhammad Rayyan <rayyanch47@gmail.com>
2024-10-22 18:31:22 +0500

    Add files via upload

---
+++ 233532_lab_05/task1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vp_programming_Lab_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Activity 1
+            double a = 98, b = 0;
+          double result = 0;
+          try
+          {
+            result = SafeDivision(a, b);
+          Console.WriteLine($"{a} divided by {b} = {result}");
+
+
+            }
+            catch (DivideByZeroException e)
+            {
+              { Console.WriteLine("Attempted divide by zero."); }
+            }
+
+            //Activity 2
+            TestCatch2();
+            Console.ReadLine();
+        }
+        //Activity 1
+        static double SafeDivision(double x, double y)
+        {
+            if (y == 0)
+                throw new System.DivideByZeroException();
+            return x / y;
+        }
+
+        //Activity 2
+        static void TestCatch2()
+        {
+            System.IO.StreamWriter sw = null;
+            try
+            {
+                sw = new System.IO.StreamWriter(@"C:\test\test.txt");
+                sw.WriteLine("Hello");
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            catch (System.IO.IOException ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                sw.Close();
+            }
+            System.Console.WriteLine("Done");
+        }
+    }
+}
