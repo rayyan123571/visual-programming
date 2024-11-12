ce2a2e0d275de6deeec74fd4507fea0966122337
Muhammad Rayyan <rayyanch47@gmail.com>
2024-11-12 17:16:55 +0500

    Add files via upload

---
+++ 233532_lab_06/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCheckContains = new System.Windows.Forms.Button();
+            this.btnDisplayAll = new System.Windows.Forms.Button();
+            this.listBoxElements = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(272, 59);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(155, 22);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(34, 317);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(137, 317);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(232, 317);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCheckContains
+            // 
+            this.btnCheckContains.Location = new System.Drawing.Point(326, 317);
+            this.btnCheckContains.Name = "btnCheckContains";
+            this.btnCheckContains.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckContains.TabIndex = 4;
+            this.btnCheckContains.Text = "Check Contains";
+            this.btnCheckContains.UseVisualStyleBackColor = true;
+            this.btnCheckContains.Click += new System.EventHandler(this.btnCheckContains_Click);
+            // 
+            // btnDisplayAll
+            // 
+            this.btnDisplayAll.Location = new System.Drawing.Point(422, 317);
+            this.btnDisplayAll.Name = "btnDisplayAll";
+            this.btnDisplayAll.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplayAll.TabIndex = 5;
+            this.btnDisplayAll.Text = "Display All";
+            this.btnDisplayAll.UseVisualStyleBackColor = true;
+            this.btnDisplayAll.Click += new System.EventHandler(this.btnDisplayAll_Click);
+            // 
+            // listBoxElements
+            // 
+            this.listBoxElements.FormattingEnabled = true;
+            this.listBoxElements.ItemHeight = 16;
+            this.listBoxElements.Location = new System.Drawing.Point(137, 141);
+            this.listBoxElements.Name = "listBoxElements";
+            this.listBoxElements.Size = new System.Drawing.Size(234, 68);
+            this.listBoxElements.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter the text:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Array list";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(229, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Windows Application of ArrayList";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxElements);
+            this.Controls.Add(this.btnDisplayAll);
+            this.Controls.Add(this.btnCheckContains);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCheckContains;
+        private System.Windows.Forms.Button btnDisplayAll;
+        private System.Windows.Forms.ListBox listBoxElements;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 233532_lab_06/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList arrayList;
+        public Form1()
+        {
+            InitializeComponent();
+            arrayList = new ArrayList();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text;  // Get text from input TextBox
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                arrayList.Add(input);  // Add element to ArrayList
+                txtInput.Clear();      // Clear the TextBox after adding
+                DisplayElements();     // Refresh the ListBox to display updated elements
+            }
+            else
+            {
+                MessageBox.Show("Please enter a value.");  // Show message if input is empty
+            }
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text;
+            if (arrayList.Contains(input))  // Check if element exists in ArrayList
+            {
+                arrayList.Remove(input);  // Remove element from ArrayList
+                txtInput.Clear();         // Clear the TextBox after removing
+                DisplayElements();        // Refresh the ListBox
+            }
+            else
+            {
+                MessageBox.Show("Element not found in the list.");  // Show message if not found
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            arrayList.Clear();  // Clear all elements in ArrayList
+            DisplayElements();  // Refresh the ListBox to show an empty list
+        }
+
+        private void btnCheckContains_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text;
+            if (arrayList.Contains(input))  // Check if element exists in ArrayList
+            {
+                MessageBox.Show("The element is present in the list.");
+            }
+            else
+            {
+                MessageBox.Show("The element is not in the list.");
+            }
+        }
+
+        private void btnDisplayAll_Click(object sender, EventArgs e)
+        {
+            if (arrayList.Count > 0)
+            {
+                // Join all elements into a single string separated by commas
+                string allElements = string.Join(", ", arrayList.Cast<string>());
+
+                // Show the list in a MessageBox
+                MessageBox.Show("Elements in the list: " + allElements, "All Elements");
+            }
+            else
+            {
+                // Show a message if the list is empty
+                MessageBox.Show("The list is currently empty.", "All Elements");
+            }
+        }
+        private void DisplayElements()
+        {
+            listBoxElements.Items.Clear();  // Clear ListBox
+            foreach (var item in arrayList)
+            {
+                listBoxElements.Items.Add(item);  // Add each ArrayList item to ListBox
+            }
+        }
+
+        
+
+       
+    }
+}
