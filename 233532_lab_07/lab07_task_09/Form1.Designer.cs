c6ff1b9ae2238d56753ac0907d0488e9b488c677
Muhammad Rayyan <rayyanch47@gmail.com>
2024-11-19 18:57:57 +0500

    Add files via upload

---
+++ 233532_lab_07/lab07_task_09/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace lab07_task_09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblCharactersLeft = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(175, 64);
+            this.txtInput.MaxLength = 160;
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(183, 22);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lblCharactersLeft
+            // 
+            this.lblCharactersLeft.AutoSize = true;
+            this.lblCharactersLeft.Location = new System.Drawing.Point(175, 124);
+            this.lblCharactersLeft.Name = "lblCharactersLeft";
+            this.lblCharactersLeft.Size = new System.Drawing.Size(123, 16);
+            this.lblCharactersLeft.TabIndex = 1;
+            this.lblCharactersLeft.Text = "Characters Left: 160";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCharactersLeft);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblCharactersLeft;
+    }
+}
+
---
+++ 233532_lab_07/lab07_task_09/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab07_task_09
+{
+    public partial class Form1 : Form
+    {
+        private const int MaxCharacters = 160;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            int remainingChars = MaxCharacters - txtInput.Text.Length;
+            lblCharactersLeft.Text = "Characters Left: " + remainingChars;
+        }
+    }
+}
