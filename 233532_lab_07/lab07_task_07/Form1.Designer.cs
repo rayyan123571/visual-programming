5fa1299037da1c0de7abcfa181c16261ea3767e6
Muhammad Rayyan <rayyanch47@gmail.com>
2024-11-19 18:03:16 +0500

    Add files via upload

---
+++ 233532_lab_07/lab07_task_07/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace lab07_task_07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(147, 98);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(38, 16);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "Time";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(203, 169);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show Time & Date";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(150, 134);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(36, 16);
+            this.dateLabel.TabIndex = 2;
+            this.dateLabel.Text = "Date";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Time An Date Show Application";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.timeLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 233532_lab_07/lab07_task_07/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab07_task_07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Display current date and time in the label
+            timeLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+
+            // Update date label every second
+            dateLabel.Text = DateTime.Now.ToString("dddd, MMMM dd, yyyy");
+        }
+    }
+}
