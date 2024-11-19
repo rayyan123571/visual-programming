e1400181e7a262218fd9a16ae0b72f1bed278328
Muhammad Rayyan <rayyanch47@gmail.com>
2024-11-19 18:42:24 +0500

    Add files via upload

---
+++ 233532_lab_07/lab07_task_08/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace lab07_task_08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.lblTotalPhotos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(164, 77);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(407, 208);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(496, 301);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Set";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(223, 301);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.ReadOnly = true;
+            this.txtLocation.Size = new System.Drawing.Size(267, 22);
+            this.txtLocation.TabIndex = 2;
+            // 
+            // lblTotalPhotos
+            // 
+            this.lblTotalPhotos.AutoSize = true;
+            this.lblTotalPhotos.Location = new System.Drawing.Point(204, 377);
+            this.lblTotalPhotos.Name = "lblTotalPhotos";
+            this.lblTotalPhotos.Size = new System.Drawing.Size(96, 16);
+            this.lblTotalPhotos.TabIndex = 3;
+            this.lblTotalPhotos.Text = "Total Photos: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 510);
+            this.Controls.Add(this.lblTotalPhotos);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label lblTotalPhotos;
+    }
+}
+
---
+++ 233532_lab_07/lab07_task_08/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace lab07_task_08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtLocation.Text = folderDialog.SelectedPath;
+                    LoadImages(folderDialog.SelectedPath);
+                }
+            }
+        }
+
+        private void LoadImages(string folderPath)
+        {
+            var files = Directory.GetFiles(folderPath, "*.jpg");
+            lblTotalPhotos.Text = "Total Photos: " + files.Length;
+
+            if (files.Length > 0)
+            {
+                pictureBox.ImageLocation = files[0];  // Preview the first image
+            }
+        }
+    }
+}
