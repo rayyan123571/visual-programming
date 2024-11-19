c0947e29428e56b6c065418683c14a7acb165515
Muhammad Rayyan <rayyanch47@gmail.com>
2024-11-19 16:58:01 +0500

    Add files via upload

---
+++ 233532_lab_07/lab07_task_04/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace lab07_task_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFahrenheit = new System.Windows.Forms.Label();
+            this.textBoxFahrenheit = new System.Windows.Forms.TextBox();
+            this.labelCelsius = new System.Windows.Forms.Label();
+            this.textBoxCelsius = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFahrenheit
+            // 
+            this.labelFahrenheit.AutoSize = true;
+            this.labelFahrenheit.Location = new System.Drawing.Point(123, 56);
+            this.labelFahrenheit.Name = "labelFahrenheit";
+            this.labelFahrenheit.Size = new System.Drawing.Size(70, 16);
+            this.labelFahrenheit.TabIndex = 0;
+            this.labelFahrenheit.Text = "Fahrenheit";
+            // 
+            // textBoxFahrenheit
+            // 
+            this.textBoxFahrenheit.Location = new System.Drawing.Point(227, 56);
+            this.textBoxFahrenheit.Name = "textBoxFahrenheit";
+            this.textBoxFahrenheit.Size = new System.Drawing.Size(100, 22);
+            this.textBoxFahrenheit.TabIndex = 1;
+            // 
+            // labelCelsius
+            // 
+            this.labelCelsius.AutoSize = true;
+            this.labelCelsius.Location = new System.Drawing.Point(123, 115);
+            this.labelCelsius.Name = "labelCelsius";
+            this.labelCelsius.Size = new System.Drawing.Size(51, 16);
+            this.labelCelsius.TabIndex = 2;
+            this.labelCelsius.Text = "Celsius";
+            // 
+            // textBoxCelsius
+            // 
+            this.textBoxCelsius.Location = new System.Drawing.Point(227, 109);
+            this.textBoxCelsius.Name = "textBoxCelsius";
+            this.textBoxCelsius.ReadOnly = true;
+            this.textBoxCelsius.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCelsius.TabIndex = 3;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(126, 166);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalculate.TabIndex = 4;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(227, 166);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxCelsius);
+            this.Controls.Add(this.labelCelsius);
+            this.Controls.Add(this.textBoxFahrenheit);
+            this.Controls.Add(this.labelFahrenheit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFahrenheit;
+        private System.Windows.Forms.TextBox textBoxFahrenheit;
+        private System.Windows.Forms.Label labelCelsius;
+        private System.Windows.Forms.TextBox textBoxCelsius;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ 233532_lab_07/lab07_task_04/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab07_task_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            // Try to parse the input Fahrenheit value
+            if (double.TryParse(textBoxFahrenheit.Text, out double fahrenheit))
+            {
+                // Convert Fahrenheit to Celsius
+                double celsius = (fahrenheit - 32) * 5 / 9;
+
+                // Display the Celsius value in the TextBox (formatted to 2 decimal places)
+                textBoxCelsius.Text = celsius.ToString("F2");
+            }
+            else
+            {
+                // Show an error message if the input is not valid
+                MessageBox.Show("Please enter a valid number for Fahrenheit.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            // Close the application
+            this.Close();
+        }
+    }
+}
