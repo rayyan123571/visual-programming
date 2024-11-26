9836ec6bef988a0cad5aa9828f9ead5b507bbbbb
Muhammad Rayyan <rayyanch47@gmail.com>
2024-11-26 23:41:54 +0500

    Add files via upload

---
+++ lab08_activity/1_10_Activities/Windowsform/1_10_Activities/Windowsform/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string name, string country, string gender, string maritalStatus, string hobbies)
+        {
+            InitializeComponent();
+
+            // Set the values to the labels
+            labelName.Text = $"Customer Name: {name}";
+            labelCountry.Text = $"Country: {country}";
+            labelGender.Text = $"Gender: {gender}";
+            labelMaritalStatus.Text = $"Marital Status: {maritalStatus}";
+            labelHobbies.Text = $"Hobbies: {hobbies}";
+        }
+    }
+}
