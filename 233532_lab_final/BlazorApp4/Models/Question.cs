eddffab31584aa31598f9932b953989e5b133445
Muhammad Rayyan <rayyanch47@gmail.com>
2025-01-07 18:03:01 +0500

    Add files via upload

---
+++ 233532_lab_final/BlazorApp4/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp4.Models
+{
+    public class Question
+    {
+        public string QuestionText { get; set; }
+        public string Option1 { get; set; }
+        public string Option2 { get; set; }
+        public string Option3 { get; set; }
+        public string Option4 { get; set; }
+        public string CorrectAnswer { get; set; }
+        public int AssignedMarks { get; set; }
+        public int TimeLimitSeconds { get; set; }
+        public string DifficultyLevel { get; set; }
+        public string Topic { get; set; }
+    }
+}
